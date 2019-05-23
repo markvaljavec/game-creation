719d569cd498a6292adad4ea1f80a69540f0ecb8
markvaljavec <mark.valjavec@gmail.com>
2019-05-23 10:51:17 +0200

    Created base class and started on Window, Game

---
+++ GameCreation/src/com/markv/game/Game.java
@@ -0,0 +1,7 @@
+package com.markv.game;
+
+public class Game {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ GameCreation/src/com/markv/game/Window.java
@@ -0,0 +1,7 @@
+package com.markv.game;
+
+import java.awt.Canvas;
+
+public class Window extends Canvas {
+
+}
