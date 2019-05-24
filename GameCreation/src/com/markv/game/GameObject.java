285c44b3eefa7db5304e5d88ffe32d7096b6c5cf
aljazmedic <aljaz.medic@gmail.com>
2019-05-24 13:52:21 +0200

    Added abstract class GameObject

---
+++ GameCreation/src/com/markv/game/GameObject.java
@@ -0,0 +1,10 @@
+package com.markv.game;
+
+public abstract class GameObject {
+	float x;
+	float y;
+	abstract void render();
+	abstract void tick();
+	abstract float getX();
+	abstract float getY();
+}
