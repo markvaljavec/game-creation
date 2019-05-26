47c528131b668172147cb6ded1e26fd4d3b99ae1
AyyItsAljo <aljaz.medic@gmail.com>
2019-05-26 10:32:44 +0200

    Added getGameObjectByID in handler, works O(n).

--- src/com/markv/game/Handler.java
+++ src/com/markv/game/Handler.java
@@ -3,31 +3,41 @@ package com.markv.game;
 import java.awt.Graphics;
 import java.util.LinkedList;
 
+import javax.lang.model.UnknownEntityException;
+
 public class Handler {
 
-	LinkedList<GameObject> object = new LinkedList<GameObject>();
+	private LinkedList<GameObject> objects = new LinkedList<GameObject>();
 	public void tick() {
-		for(int i = 0; i < object.size(); i++) {
-			GameObject tempObject = object.get(i);
+		for(int i = 0; i < objects.size(); i++) {
+			GameObject tempObject = objects.get(i);
 			
 			tempObject.tick();
 		}
 	}
 	
 	public void render(Graphics g) {
-		for(int i = 0; i < object.size(); i++) {
-			GameObject tempObject = object.get(i);
+		for(int i = 0; i < objects.size(); i++) {
+			GameObject tempObject = objects.get(i);
 			
 			tempObject.render(g);
 		}
 	}
 	
 	public void addObject(GameObject object) {
-		this.object.add(object);
+		this.objects.add(object);
 	}
 	
 	public void removeObject(GameObject object) {
-		this.object.remove(object);
+		this.objects.remove(object);
+	}
+	public GameObject getGameObjectByID(ID id) throws Exception {
+		for(int i = 0; i < this.objects.size(); i++) {
+			if(objects.get(i).getID() == id) {
+				return objects.get(i);
+			}
+		}
+		throw new Exception("No such id.");
 	}
 }
 
--- src/com/markv/game/KeyInput.java
+++ src/com/markv/game/KeyInput.java
@@ -14,31 +14,28 @@ public class KeyInput extends KeyAdapter {
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		
-		for(int i = 0; i < handler.object.size(); i++) {
-			GameObject tempObject = handler.object.get(i);
-			if(tempObject.getID() == ID.Player) {
-				// key events for player1
-				if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
-			}
-		}
+		GameObject tempObject;
+		try {
+			tempObject = handler.getGameObjectByID(ID.Player);
+			if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
+			if(key == KeyEvent.VK_S) tempObject.setVelY(5);
+			if(key == KeyEvent.VK_D) tempObject.setVelX(5);
+			if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
+		} catch (Exception e1) {}
+
 	}
 	
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		
-		for(int i = 0; i < handler.object.size(); i++) {
-			GameObject tempObject = handler.object.get(i);
-			
-			if(tempObject.getID() == ID.Player) {
-				// key events for player1
-				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
-			}
-		}
+		GameObject tempObject;
+		try {
+			tempObject = handler.getGameObjectByID(ID.Player);
+			if(key == KeyEvent.VK_W) tempObject.setVelY(0);
+			if(key == KeyEvent.VK_S) tempObject.setVelY(0);
+			if(key == KeyEvent.VK_D) tempObject.setVelX(0);
+			if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+		} catch (Exception e1) {}
+		
 	}	
 }
